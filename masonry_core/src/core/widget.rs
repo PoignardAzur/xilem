abcc1d05aaa3c41124a88a63bfaf1ec11f775d78
Olivier FAURE <couteaubleu@gmail.com>
2026-02-18 13:54:14 +0100

    Fix more build errors

--- masonry_core/src/core/widget.rs
+++ masonry_core/src/core/widget.rs
@@ -538,7 +538,7 @@ pub trait Widget: AsDynWidget + Any {
         NewWidget::new(self)
     }
 
-    /// Convenience method to wrap this in a [`NewWidget`] with the given [`Properties`].
+    /// Convenience method to wrap this in a [`NewWidget`] with the given [`PropertySet`].
     fn with_props(self, props: impl Into<PropertySet>) -> NewWidget<Self>
     where
         Self: Sized,
